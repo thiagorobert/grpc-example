6a41d53dcbb14b441beed18d23832b6cdd538d02
thiago <thiagorobert@gmail.com>
2022-01-13 16:11:56 -0800

    Improve input flags handling using module 'argparse'.

--- src/python_grpc/client.py
+++ src/python_grpc/client.py
@@ -1,34 +1,42 @@
+import argparse
+import sys
 import grpc
 import proto.unary_pb2_grpc as pb2_grpc
 import proto.unary_pb2 as pb2
 
 
+def inputFlags():
+    parser = argparse.ArgumentParser(description='gRPC example client')
+    parser.add_argument(
+        '--server_host', dest='server_host', default='localhost', help='server host')
+    parser.add_argument(
+        '--server_port', dest='server_port', default='8080', help='server port')
+    return parser.parse_args()
+
+
 class UnaryClient(object):
-    """
-    Client for gRPC functionality
-    """
+    """gRPC client."""
 
-    def __init__(self):
-        self.host = 'localhost'
-        self.server_port = 8080
+    def __init__(self, server_host, server_port):
+        host_port = '{}:{}'.format(server_host, server_port)
 
-        # instantiate a channel
-        self.channel = grpc.insecure_channel(
-            '{}:{}'.format(self.host, self.server_port))
+        # Instantiate channel.
+        self.channel = grpc.insecure_channel(host_port)
 
-        # bind the client and the server
+        # Bind client and server.
         self.stub = pb2_grpc.UnaryStub(self.channel)
 
-    def get_url(self, message):
-        """
-        Client function to call the rpc for GetServerResponse
-        """
+    def Run(self, message):
+        """Issues GetServerResponse RPC."""
         message = pb2.Message(message=message)
-        print(f'{message}')
         return self.stub.GetServerResponse(message)
 
 
 if __name__ == '__main__':
-    client = UnaryClient()
-    result = client.get_url(message="Hello Server you there?")
+    flags = inputFlags()
+
+    print(f'Client talking to "{flags.server_host}:{flags.server_port}"')
+
+    client = UnaryClient(flags.server_host, flags.server_port)
+    result = client.Run(message='Hello Server you there?')
     print(f'{result}')
--- src/python_grpc/server.py
+++ src/python_grpc/server.py
@@ -1,9 +1,18 @@
+import argparse
+import sys
 import grpc
 from grpc_reflection.v1alpha import reflection
 from concurrent import futures
 import time
 import proto.unary_pb2_grpc as pb2_grpc
 import proto.unary_pb2 as pb2
+
+
+def inputFlags():
+    parser = argparse.ArgumentParser(description='gRPC example server')
+    parser.add_argument(
+        '--server_port', dest='server_port', default='8080', help='server port')
+    return parser.parse_args()
 
 
 class UnaryService(pb2_grpc.UnaryServicer):
@@ -20,23 +29,31 @@ class UnaryService(pb2_grpc.UnaryServicer):
 
         return pb2.MessageResponse(**result)
 
+
 # See https://github.com/grpc/grpc/blob/master/doc/python/server_reflection.md
 def enableReflectionAPI(server):
-    SERVICE_NAMES = (
+    service_names = (
         pb2.DESCRIPTOR.services_by_name['Unary'].full_name,
         reflection.SERVICE_NAME,
     )
-    reflection.enable_server_reflection(SERVICE_NAMES, server)
+    reflection.enable_server_reflection(service_names, server)
 
-def serve():
+
+def serve(server_port):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     enableReflectionAPI(server)
 
     pb2_grpc.add_UnaryServicer_to_server(UnaryService(), server)
-    server.add_insecure_port('[::]:8080')
+
+    server.add_insecure_port('[::]:' + server_port)
+
     server.start()
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    serve()
+    flags = inputFlags()
+
+    print(f'Server listening at ":{flags.server_port}"')
+
+    serve(flags.server_port)
